3734f5712a6386db34793abf76fe846f94435299
MoshPe <fghghf98@gmail.com>
2021-12-18 23:59:46 +0200

    .

--- src/logic/Employee.java
+++ src/logic/Employee.java
@@ -2,12 +2,10 @@ package logic;
 
 public class Employee {
 	private String name;
-	private int w4c_card;
 	private boolean isApproved;
-	
+
 	public Employee(String name, int w4c_card, boolean isApproved) {
 		this.name = name;
-		this.w4c_card = w4c_card;
 		this.isApproved = isApproved;
 	}
 	
@@ -16,12 +14,6 @@ public class Employee {
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public int getW4c_card() {
-		return w4c_card;
-	}
-	public void setW4c_card(int w4c_card) {
-		this.w4c_card = w4c_card;
 	}
 	public boolean isApproved() {
 		return isApproved;
